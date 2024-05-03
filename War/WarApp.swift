49d58a47d95b6b9137fc4eb528e8f4699d94e301
rrvillareal23 <rrvillareal@arizona.edu.com>
2024-05-03 16:45:53 -0700

    Initial Commit

---
+++ War/WarApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarApp.swift
+//  War
+//
+//  Created by Ricky Villareal on 5/3/24.
+//
+
+import SwiftUI
+
+@main
+struct WarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
