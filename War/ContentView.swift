6633ce104d567dc1c2d85657af2caa1f171a2eba
rrvillareal23 <rrvillareal@arizona.edu.com>
2024-05-12 12:51:16 -0700

    War game

--- War/ContentView.swift
+++ War/ContentView.swift
@@ -8,16 +8,94 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var playerCard = "back"
+    @State var cpuCard = "back"
+
+    @State var playerScore = 0
+    @State var cpuScore = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        ZStack{
+            Image(.backgroundPlain)
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack{
+                Spacer()
+                Image(.logo)
+                Spacer()
+                
+                HStack{
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                }
+                Spacer()
+                
+                Button(action: {
+                    deal()
+                }, label: {
+                    Image(.button)
+                })
+
+                Spacer()
+                
+                HStack{
+                    Spacer()
+                    VStack{
+                        Text("Player")
+                            .font(.headline)
+                            .padding(.bottom,10.0)
+                        Text(String(playerScore))
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                    
+                    VStack{
+                        Text("CPU")
+                            .font(.headline)
+                            .padding(.bottom,10.0)
+
+                        Text(String(cpuScore))
+                            .font(.largeTitle)
+
+                    }
+                    Spacer()
+                }
+                .foregroundColor(.white)
+                Spacer()
+            }
         }
-        .padding()
+    }
+    
+    func deal(){
+        // Randomize the players card
+        let playerCardValue = Int.random(in: 2...14)
+        playerCard = "card" + String(playerCardValue)
+        
+        // Randomize the cpus card
+        let cpuCardValue = Int.random(in: 2...14)
+        cpuCard = "card" + String(cpuCardValue)
+
+        
+        // Update score
+        if playerCardValue > cpuCardValue {
+            playerScore += 1
+        }
+        else if cpuCardValue > playerCardValue{
+            cpuScore += 1
+        }
+        else{
+            // tie, do nothing
+        }
+        
     }
 }
+
+
 
 #Preview {
     ContentView()
